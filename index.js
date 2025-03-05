09ae905cc146521438fae5661e447a983b2aeb0b
pawin.sukta@gmail.com <pawin.sukta@gmail.com>
2025-03-05 14:26:56 +0700

    edit cors

--- index.js
+++ index.js
@@ -15,10 +15,19 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+})
+
 app.use('/api/rooms', roomRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/user', userRoutes);
-app.use(cors());
+
 
 app.listen(PORT, () => {
     connectDB();
